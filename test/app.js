04f6f55d209ec9e53280fd9bdd3e236b65bfa70d
alifianadexe <alifianadexe@gmail.com>
2024-03-02 22:22:52 +0700

    change some format

--- test/app.js
+++ test/app.js
@@ -4,27 +4,27 @@ var assert = require('assert')
 var express = require('..')
 var request = require('supertest')
 
-describe('app', function(){
-  it('should inherit from event emitter', function(done){
+describe('app', function () {
+  it('should inherit from event emitter', function (done) {
     var app = express();
     app.on('foo', done);
     app.emit('foo');
   })
 
-  it('should be callable', function(){
+  it('should be callable', function () {
     var app = express();
     assert.equal(typeof app, 'function');
   })
 
-  it('should 404 without routes', function(done){
+  it('should 404 without routes', function (done) {
     request(express())
-    .get('/')
-    .expect(404, done);
+      .get('/')
+      .expect(404, done);
   })
 })
 
-describe('app.parent', function(){
-  it('should return the parent when mounted', function(){
+describe('app.parent', function () {
+  it('should return the parent when mounted', function () {
     var app = express()
       , blog = express()
       , blogAdmin = express();
@@ -38,8 +38,8 @@ describe('app.parent', function(){
   })
 })
 
-describe('app.mountpath', function(){
-  it('should return the mounted path', function(){
+describe('app.mountpath', function () {
+  it('should return the mounted path', function () {
     var admin = express();
     var app = express();
     var blog = express();
@@ -56,20 +56,20 @@ describe('app.mountpath', function(){
   })
 })
 
-describe('app.router', function(){
-  it('should throw with notice', function(done){
+describe('app.router', function () {
+  it('should throw with notice', function (done) {
     var app = express()
 
     try {
       app.router;
-    } catch(err) {
+    } catch (err) {
       done();
     }
   })
 })
 
-describe('app.path()', function(){
-  it('should return the canonical', function(){
+describe('app.path()', function () {
+  it('should return the canonical', function () {
     var app = express()
       , blog = express()
       , blogAdmin = express();
@@ -83,7 +83,7 @@ describe('app.path()', function(){
   })
 })
 
-describe('in development', function(){
+describe('in development', function () {
   before(function () {
     this.env = process.env.NODE_ENV
     process.env.NODE_ENV = 'development'
@@ -93,13 +93,13 @@ describe('in development', function(){
     process.env.NODE_ENV = this.env
   })
 
-  it('should disable "view cache"', function(){
+  it('should disable "view cache"', function () {
     var app = express();
     assert.ok(!app.enabled('view cache'))
   })
 })
 
-describe('in production', function(){
+describe('in production', function () {
   before(function () {
     this.env = process.env.NODE_ENV
     process.env.NODE_ENV = 'production'
@@ -109,13 +109,13 @@ describe('in production', function(){
     process.env.NODE_ENV = this.env
   })
 
-  it('should enable "view cache"', function(){
+  it('should enable "view cache"', function () {
     var app = express();
     assert.ok(app.enabled('view cache'))
   })
 })
 
-describe('without NODE_ENV', function(){
+describe('without NODE_ENV', function () {
   before(function () {
     this.env = process.env.NODE_ENV
     process.env.NODE_ENV = ''
@@ -125,7 +125,7 @@ describe('without NODE_ENV', function(){
     process.env.NODE_ENV = this.env
   })
 
-  it('should default to development', function(){
+  it('should default to development', function () {
     var app = express();
     assert.strictEqual(app.get('env'), 'development')
   })
